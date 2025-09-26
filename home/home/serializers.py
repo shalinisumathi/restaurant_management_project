275392982d90287e84e662e31ea643e6e5dfc1d5
shalinisumathi <shalinipriyasumathi2005@gmail.com>
2025-09-26 20:02:28 +0530

    51046

---
+++ home/home/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import MenuCategory
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = ['name'] 
