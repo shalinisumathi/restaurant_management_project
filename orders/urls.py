b9487b3d873c14bdaa6ed4d2d845863450c5782e
shalinisumathi <shalinipriyasumathi2005@gmail.com>
2025-09-29 22:19:10 +0530

    51843

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import OrderHistoryView
 
 urlpatterns = [
-    
+    path('order-history/', OrderHistoryView.as_view(), name='order-history'),
 ]
