f9fcbaf202a05cd4d3b7083f69bce00e9acfa4fd
shalinisumathi <shalinipriyasumathi2005@gmail.com>
2025-09-29 21:51:32 +0530

    51842

--- menu/urls.py
+++ menu/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MenuItemVIewSet
+from .views import MenuItemsByCategoryView
+
 
 router = DefaultRouter()
 router.register(r'menu-items', MenuItemVIewSet, basename='menuitem')
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+urlpatterns1 = [
+    path('menu-items/by-category/', MenuItemsByCategoryView.as_view(), name='menu-items-by-category'),
 ]
