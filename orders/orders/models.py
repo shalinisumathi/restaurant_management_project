bf35484dc715f8338c21ff8bf47ebefa5104025c
shalinisumathi <shalinipriyasumathi2005@gmail.com>
2025-10-01 20:46:03 +0530

    52493

--- orders/orders/models.py
+++ orders/orders/models.py
@@ -1,21 +1,10 @@
 from django.db import models
-class ActiveOrderManager(models.Manager):
-    def get_active_orders(self):
-        return self.filter(status__in=['pending', 'processing'])
+class Coupon(models.model):
+    code = models.charField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_degits=5, decimal_place=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
 
-class Order(models.model):
-    STATUS_CHOICES = [
-        ('pending', 'pending'),
-        ('processing', 'processing'),
-        ('completed', 'completed'),
-        ('cancelled', 'cancelled'),
-]
-status = models.charField(max_length=20, choices=STATUS_CHOICES)
-total_price = models.DecimalField(max_digits=10, decimal_places=2)
-created_at = models.DateTimeField(auto_now_add=True)
-objects = ActiveOrderManager
-
-order.objects.get_active_orders().filter(total_price__gt=500)
-from orders.models import order
-active_orders = order . objects.get_active_orders()
-print(active_orders)
+    def __str__(self):
+        return f"{self.code} - {self.discount_percentage}%"
