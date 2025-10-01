6ab876ab56fc67ca31028eddbca2a49435eff650
shalinisumathi <shalinipriyasumathi2005@gmail.com>
2025-10-01 21:08:32 +0530

    52493

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response 
-from rest_framework.permissions import IsAuthenticated
-from .models import Order
-from .serializers import OrderSerializer
+from rest _framework.views import APIView
+from rest_framework.response import response
+from rest_framework import status
+from django.utils import timezone
+from .models import Coupon
 
-class OrderHistoryView(APIView):
-    permission_classes = [IsAuthenticated]
+class CouponValidationView(APIView):
+    def post(self, request):
+        code = request.data.get("code")
+        if not code:
+            return Response({"error": "Coupon code is required"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            Coupon = Coupon.objects.get(code=code)
+        except Coupon.DoesNotExist:
+            return Response({"error": "Invalid coupon code"}, status=status.HTTP_400_BAD_REQUEST)
+        today = timezone.now().date()
 
-    def get(self, request):
-        orders = Order.objects.filter(user=request.user).order_by('-created_at')
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
+        if not coupon.is_active:
+            return Response({"error": "Coupon is not active"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if not (coupon.valid_from <= today <= coupon.valid_until):
+            return Response({"error": "coupon is expired or not yet valid"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({
+            "success": True,
+            "code": coupon.code
+            "discound_percentage": float(coupon.discound_percentage)
+        }, status=status.HTTP_200_OK)
